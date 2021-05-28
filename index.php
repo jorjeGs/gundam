23f66779d68a1dacd062d28bd47eb27701262158
Jorge Garcia <76033387+jorjeGs@users.noreply.github.com>
2021-05-28 00:53:18 -0700

    referencia a about us actualizada

--- index.php
+++ index.php
@@ -74,7 +74,7 @@
 				<div class="col-lg-6">
 					<img src="resources/bannerpc.png" width="400" class="img-fluid">
 				</div>
-				<button onclick="location.href='index.php'">Conoce</button>
+				<button onclick="location.href='aboutUs.html'">Conoce</button>
 			</div>
 		</div>
 	</div>
